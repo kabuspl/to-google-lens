d40668f56fffe87ed99cab63715bc5b954564f3f
kabuspl <kabuspl@outlook.com>
2022-07-25 23:23:08 +0200

    add loading screen

--- bg.js
+++ bg.js
@@ -65,15 +65,19 @@ function searchBlob (img, msg) {
     imgEl.src = img;
 }
 
-async function search(image) {
-    let data = new FormData();
-    data.append('encoded_image', image, "screenshot.png");
-    let req = await fetch("https://lens.google.com/upload?ep=ccm&s=&st=" + Date.now(), {
-        referrer: '',
-        mode: 'cors',
-        method: 'POST',
-        body: data
-    });
-    let response = await req.text();
-    browser.tabs.create({ url: response.match(/<meta .*URL=(https?:\/\/.*)"/)[1] });
+function search(image) {
+    browser.tabs.query({active: true}).then(active=>{
+        browser.tabs.create({url: "loading.html", index: active[0].index+1}).then(async tab=>{
+            let data = new FormData();
+            data.append('encoded_image', image, "screenshot.png");
+            let req = await fetch("https://lens.google.com/upload?ep=ccm&s=&st=" + Date.now(), {
+                referrer: '',
+                mode: 'cors',
+                method: 'POST',
+                body: data
+            });
+            let response = await req.text();
+            browser.tabs.update(tab.id,{ url: response.match(/<meta .*URL=(https?:\/\/.*)"/)[1] });
+        });
+    })
 }

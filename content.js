a9488fcfaf300cf27a4291837c7717beb61ceaa4
kabuspl <kabuspl@outlook.com>
2022-10-18 01:10:32 +0200

    add touchscreen support

--- content.js
+++ content.js
@@ -24,51 +24,77 @@ down = false,
     startX = 0,
     startY = 0;
 
-cont.addEventListener("mousedown", e => {
+function startSelection(x,y) {
     down = true;
-    selector.style.left = e.clientX + "px";
-    selector.style.top = e.clientY + "px";
-    startX = e.clientX;
-    startY = e.clientY;
+    selector.style.left = x + "px";
+    selector.style.top = y + "px";
+    startX = x;
+    startY = y;
+}
+
+cont.addEventListener("mousedown", e => {
+    startSelection(e.clientX,e.clientY);
 });
 
-cont.addEventListener("mousemove", e => {
+cont.addEventListener("touchstart", e => {
+    startSelection(e.touches[0].clientX,e.touches[0].clientY);
+});
+
+function moveSelection(x,y) {
     if (down) {
-        if (e.clientX - startX < 0) {
-            selector.style.left = e.clientX + "px";
-            selector.style.width = (startX - e.clientX) + "px";
+        if (x - startX < 0) {
+            selector.style.left = x + "px";
+            selector.style.width = (startX - x) + "px";
         } else {
-            selector.style.width = (e.clientX - startX) + "px";
+            selector.style.width = (x - startX) + "px";
         }
-        if (e.clientY - startY < 0) {
-            selector.style.top = e.clientY + "px";
-            selector.style.height = (startY - e.clientY) + "px";
+        if (y - startY < 0) {
+            selector.style.top = y + "px";
+            selector.style.height = (startY - y) + "px";
         } else {
-            selector.style.height = (e.clientY - startY) + "px";
+            selector.style.height = (y - startY) + "px";
         }
     }
-})
+}
 
-cont.addEventListener("mouseup", e => {
+cont.addEventListener("mousemove", e => {
+    moveSelection(e.clientX, e.clientY);
+});
+
+cont.addEventListener("touchmove", e => {
+    moveSelection(e.touches[0].clientX, e.touches[0].clientY);
+    e.preventDefault();
+});
+
+function stopSelection(x,y) {
     down = false;
     let x1, y1, x2, y2;
-    if (e.clientX - startX < 0) {
-        x1 = e.clientX;
+    if (y - startX < 0) {
+        x1 = x;
         x2 = startX;
     } else {
         x1 = startX;
-        x2 = e.clientX;
+        x2 = x;
     }
-    if (e.clientY - startY < 0) {
-        y1 = e.clientY;
+    if (y - startY < 0) {
+        y1 = y;
         y2 = startY;
     } else {
         y1 = startY;
-        y2 = e.clientY;
+        y2 = y;
     }
     browser.runtime.sendMessage({ x1: x1 * window.devicePixelRatio, y1: y1 * window.devicePixelRatio, x2: x2 * window.devicePixelRatio, y2: y2 * window.devicePixelRatio });
     selector.remove();
     cont.remove();
+}
+
+cont.addEventListener("mouseup", e => {
+    stopSelection(e.clientX, e.clientY);
+});
+
+cont.addEventListener("touchend", e => {
+    stopSelection(e.changedTouches[0].clientX, e.changedTouches[0].clientY);
+    e.preventDefault();
 });
 
 window.addEventListener ('keyup', e => {
